76354fdbb9b19e9e7e90d744b65dcbacf4d968f8
Ritik Srivastava <153807650+RitikSrivastavaIT@users.noreply.github.com>
2025-04-21 15:01:38 +0530

    moved files in the newly created folder so deleting from here.

--- backend_ microservices/user-service/src/main/java/com/ritik/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ritik.repository;
-
-import com.ritik.modal.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
